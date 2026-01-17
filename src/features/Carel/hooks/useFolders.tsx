02edcfd3805f348562a987e4eeec2d9f8e6b52aa
Nicolas_dmb <gyfeix@gmail.com>
2026-01-17 20:23:12 +0100

    carel : new folders

--- src/features/Carel/hooks/useFolders.tsx
+++ src/features/Carel/hooks/useFolders.tsx
@@ -5,7 +5,7 @@ import {useNavigate} from "react-router-dom"
 
 export default function useFolders() {
     const concordeRef = useRef<HTMLImageElement | null>(null);
-    const apcRef = useRef<HTMLImageElement | null>(null);
+    const bossaRef = useRef<HTMLImageElement | null>(null);
     const maroquinerieRef = useRef<HTMLImageElement | null>(null);
     const chaussuresRef = useRef<HTMLImageElement | null>(null);
     const alesiaRef = useRef<HTMLImageElement | null>(null);
@@ -14,6 +14,14 @@ export default function useFolders() {
     const closeFoldersRef = useRef<HTMLImageElement | null>(null);
     const currentRef = useRef<HTMLImageElement | null>(null);
     const clemenceauRef = useRef<HTMLImageElement | null>(null);
+    const accessoiresLabelRef = useRef<HTMLImageElement | null>(null);
+    const accessoiresRef = useRef<HTMLImageElement | null>(null);
+    const miniClemenceauRef = useRef<HTMLImageElement | null>(null);
+    const madeleineRef = useRef<HTMLImageElement | null>(null);
+    const madeleineBrodeeRef = useRef<HTMLImageElement | null>(null);
+    const maddieRef = useRef<HTMLImageElement | null>(null);
+    const mabillonRef = useRef<HTMLImageElement | null>(null);
+    const elyseeRef = useRef<HTMLImageElement | null>(null);
 
     const navigate = useNavigate()
 
@@ -32,14 +40,22 @@ export default function useFolders() {
         const footerEl = document.getElementById("footer");
         const storage = StorageRef.current;
         const concorde = concordeRef.current;
-        const apc = apcRef.current;
+        const bossa = bossaRef.current;
         const alesia = alesiaRef.current;
         const maroquinerie = maroquinerieRef.current;
         const chaussures = chaussuresRef.current;
         const closeFolders = closeFoldersRef.current;
         const clemenceau = clemenceauRef.current;
+        const accessoiresLabel = accessoiresLabelRef.current;
+        const accessoires = accessoiresRef.current;
+        const miniClem = miniClemenceauRef.current;
+        const madeleine = madeleineRef.current;
+        const madeleineBrodee = madeleineBrodeeRef.current;
+        const maddie = maddieRef.current;
+        const mabillon = mabillonRef.current;
+        const elysee = elyseeRef.current;
 
-        if (!footerEl || !storage || !concorde || !apc || !alesia || !maroquinerie || !chaussures || !closeFolders || !clemenceau) return;
+        if (!footerEl || !storage || !concorde || !bossa || !alesia || !maroquinerie || !chaussures || !closeFolders || !clemenceau || !accessoiresLabel || !accessoires || !miniClem || !madeleine || !madeleineBrodee || !maddie || !mabillon || !elysee) return;
 
         // Reset opacity on wrapper when go back from Carel subpage
         const wrapperEl = document.getElementById("global-wrapper");
@@ -53,28 +69,36 @@ export default function useFolders() {
 
         const ctx = gsap.context(() => {
             
-            const all = [storage, concorde, apc, alesia, chaussures, maroquinerie, closeFolders, clemenceau];
-            const folderelements = [concorde, apc, alesia, chaussures, maroquinerie, clemenceau];
+            const all = [storage, concorde, bossa, alesia, chaussures, maroquinerie, closeFolders, clemenceau, accessoiresLabel, accessoires, miniClem, madeleine, madeleineBrodee, maddie, mabillon, elysee];
+            const folderelements = [concorde, bossa, alesia, chaussures, maroquinerie, clemenceau, accessoiresLabel, accessoires, miniClem, madeleine, madeleineBrodee, maddie, mabillon, elysee];
 
-            _setGsapDefaults({ concorde, chaussures, apc, maroquinerie, alesia, closeFolders, storage, all, folderelements, clemenceau });
+            _setGsapDefaults({ concorde, chaussures, bossa, maroquinerie, alesia, closeFolders, storage, all, folderelements, clemenceau, accessoiresLabel, accessoires, miniClem, madeleine, madeleineBrodee, maddie, mabillon, elysee });
 
             const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
             
             // 1) Entry animation
             tl.to(storage, { y: 400, width: "80vw", duration: 1 }, 0);
-            tl.to(closeFolders, { y: 300, width: "75vw", duration: 1 }, 0);
+            tl.to(closeFolders, { y: 330, width: "75vw", duration: 1 }, 0);
 
             // 2) replace folders
             tl.to(closeFolders, {opacity:0, duration:0}, 1);
             tl.to(folderelements, {opacity:1, duration:0},1);
 
             // 3) staggered rise
-            tl.to(concorde, { y: 270, duration: 0.4 }, 1.0);
-            tl.to(chaussures, { y: 210, duration: 0.4 }, 1.04);
-            tl.to(apc, { y: 245, duration: 0.4 }, 1.08);
-            tl.to(maroquinerie, { y: 210, duration: 0.4 }, 1.12);
-            tl.to(alesia, { y: 220, duration: 0.4 }, 1.16);
-            tl.to(clemenceau, { y: 220, duration: 0.4 }, 1.20);
+            tl.to(accessoiresLabel, { y: 290, duration: 0.4 }, 1);
+            tl.to(accessoires, { y: 300, duration: 0.4 }, 1.04);
+            tl.to(chaussures, { y: 240, duration: 0.4 }, 1.08);
+            tl.to(bossa, { y: 280, duration: 0.4 }, 1.12);
+            tl.to(maroquinerie, { y: 250, duration: 0.4 }, 1.16);
+            tl.to(concorde, { y: 270, duration: 0.4 }, 1.20);
+            tl.to(clemenceau, { y: 260, duration: 0.4 }, 1.24);
+            tl.to(miniClem, { y: 250, duration: 0.4 }, 1.28);
+            tl.to(alesia, { y: 235, duration: 0.4 }, 1.32);
+            tl.to(madeleine, { y: 230, duration: 0.4 }, 1.36);
+            tl.to(maddie, { y: 220, duration: 0.4 }, 1.40);
+            tl.to(madeleineBrodee, { y: 200, duration: 0.4 }, 1.44);
+            tl.to(elysee, { y: 185, duration: 0.4 }, 1.48);
+            tl.to(mabillon, { y: 170, duration: 0.4 }, 1.52);
 
             tl.eventCallback("onComplete", () => {
                 _setupFolderInteractions();
@@ -115,23 +139,42 @@ export default function useFolders() {
     function _setupFolderInteractions() {
         isReady.current = true;
 
-        const c = concordeRef.current!;
-        const a = apcRef.current!;
+        const a = accessoiresRef.current!;
+        const alabel = accessoiresLabelRef.current!;
+        const concorde = concordeRef.current!;
+        const b = bossaRef.current!;
         const al = alesiaRef.current!;
         const m = maroquinerieRef.current!;
         const ch = chaussuresRef.current!;
         const clem = clemenceauRef.current!;
+        const miniClem = miniClemenceauRef.current!;
+        const madeleine = madeleineRef.current!;
+        const maddie = maddieRef.current!;
+        const madeleineBrodee = madeleineBrodeeRef.current!;
+        const elysee = elyseeRef.current!;
+        const mabillon = mabillonRef.current!;
 
-        [c, ch, a, m, al, clem].forEach((el) => baseY.current.set(el, gsap.getProperty(el, "y") as number));
 
-        _prepareAlpha(c);
+        [a, alabel, concorde, ch, b, m, al, clem, miniClem, madeleine, maddie, madeleineBrodee, elysee, mabillon].forEach((el) => baseY.current.set(el, gsap.getProperty(el, "y") as number));
+
         _prepareAlpha(a);
+        _prepareAlpha(alabel);
+        _prepareAlpha(concorde);
+        _prepareAlpha(b);
         _prepareAlpha(al);
         _prepareAlpha(m);
         _prepareAlpha(ch);
         _prepareAlpha(clem);
+        _prepareAlpha(miniClem);
+        _prepareAlpha(madeleine);
+        _prepareAlpha(maddie);
+        _prepareAlpha(madeleineBrodee);
+        _prepareAlpha(elysee);
+        _prepareAlpha(mabillon);
+        
+        // accessoiresLabel, accessoires, chaussures, bossa, maroquinerie, concorde, clemenceau, miniClemenceau, alesia, madeleine, maddie, madeleineBrodee, elysee, mabillon
 
-        const ORDER = [c, ch,  a, m, al, clem]; 
+        const ORDER = [alabel, a, ch, b, m, concorde,clem, miniClem, al, madeleine, maddie, madeleineBrodee, elysee, mabillon]; 
 
 
         const target = windowRef.current!;
@@ -234,13 +277,21 @@ export default function useFolders() {
     return {
         windowRef,
         concordeRef,
-        apcRef,
+        bossaRef,
         alesiaRef,
         StorageRef,
         closeFoldersRef, 
         maroquinerieRef,
         chaussuresRef,
         clemenceauRef,
+        accessoiresLabelRef, 
+        accessoiresRef,
+        miniClemenceauRef, 
+        madeleineRef, 
+        madeleineBrodeeRef, 
+        maddieRef, 
+        mabillonRef, 
+        elyseeRef
     };
 }
 
@@ -279,25 +330,41 @@ function _footerStyleRestore(footerEl:HTMLElement, prev: any){
 interface GsapDefaults {
     concorde: HTMLImageElement;
     chaussures: HTMLImageElement;
-    apc: HTMLImageElement;
+    bossa: HTMLImageElement;
     maroquinerie: HTMLImageElement;
     alesia: HTMLImageElement;
     closeFolders: HTMLImageElement;
     storage: HTMLImageElement;
     clemenceau: HTMLImageElement;
+    accessoiresLabel: HTMLImageElement;
+    accessoires: HTMLImageElement;
+    miniClem: HTMLImageElement;
+    madeleine: HTMLImageElement;
+    madeleineBrodee: HTMLImageElement;
+    maddie: HTMLImageElement;
+    mabillon: HTMLImageElement;
+    elysee: HTMLImageElement;
     all: HTMLImageElement[];
     folderelements: HTMLImageElement[];
 }
 
-function _setGsapDefaults({ concorde, chaussures, apc, maroquinerie, alesia, closeFolders, storage, clemenceau, all, folderelements }: GsapDefaults) { 
-    gsap.set(concorde, { zIndex: 100 });
-    gsap.set(chaussures, { zIndex: 90 });
-    gsap.set(apc, { zIndex: 80 });
-    gsap.set(maroquinerie, { zIndex: 70 });
-    gsap.set(alesia, { zIndex: 60 });
-    gsap.set(clemenceau, { zIndex: 40 });
-    gsap.set(closeFolders, { zIndex: 50 });
+function _setGsapDefaults({ concorde, chaussures, bossa, maroquinerie, alesia, closeFolders, storage, clemenceau, all, folderelements, accessoiresLabel, accessoires, miniClem, madeleine, madeleineBrodee, maddie, mabillon, elysee }: GsapDefaults) { 
     gsap.set(storage, { zIndex: 120 });
+    gsap.set(accessoiresLabel, { zIndex: 115 });
+    gsap.set(accessoires, { zIndex: 110 });
+    gsap.set(chaussures, { zIndex: 100 });
+    gsap.set(bossa, { zIndex: 90 });
+    gsap.set(maroquinerie, { zIndex: 80 });
+    gsap.set(concorde, { zIndex: 70 });
+    gsap.set(clemenceau, { zIndex: 60 });
+    gsap.set(miniClem, { zIndex: 50 });
+    gsap.set(alesia, { zIndex: 40 });
+    gsap.set(madeleine, { zIndex: 30 });
+    gsap.set(maddie, { zIndex: 25 });
+    gsap.set(madeleineBrodee, { zIndex: 20 });    
+    gsap.set(elysee, { zIndex: 15 });
+    gsap.set(mabillon, { zIndex: 10 });
+    gsap.set(closeFolders, { zIndex: 55 });
     gsap.set(all, {
         position: "fixed",
         left: "50%",
@@ -309,8 +376,8 @@ function _setGsapDefaults({ concorde, chaussures, apc, maroquinerie, alesia, clo
         backfaceVisibility: "hidden",
         transformPerspective: 1000,
     });
-    gsap.set(folderelements, { opacity: 0, y: 300, width: "75vw"});
-    gsap.set(maroquinerie,{y: 290});
-    gsap.set(closeFolders, {width: "35vw", y: -100});
+    gsap.set(folderelements, { opacity: 0, y: 330, width: "75vw"});
+    gsap.set(maroquinerie,{y: 320});
+    gsap.set(closeFolders, {width: "35vw", y: -80});
     gsap.set(storage, { y: -60, width: "35vw" });
 }
--- src/pages/carel.tsx
+++ src/pages/carel.tsx
@@ -1,14 +1,26 @@
 import { useEffect } from "react";
 import Colors from "../entities/Background.ts";
-import Storage from "../features/Carel/static/images/storage.png";
-import Alesia from "../features/Carel/static/images/alesia.png";
-import APC from "../features/Carel/static/images/apc.png";
-import Concorde from "../features/Carel/static/images/concorde.png";
+import accessoires_folder from "../features/Carel/static/images/storage/accessoires_folder.png";
+import accessoires from "../features/Carel/static/images/storage/accessoires.png";
+import Storage from "../features/Carel/static/images/storage/storage.png";
+import Alesia from "../features/Carel/static/images/storage/alesia.png";
+import Bossa from "../features/Carel/static/images/storage/bossa.png";
+import Concorde from "../features/Carel/static/images/storage/concorde.png";
 import useFolders from "../features/Carel/hooks/useFolders.tsx";
-import maroquinerie from "../features/Carel/static/images/maroquinerie.png";
-import chaussures from "../features/Carel/static/images/chaussures.png";
-import close_folders from "../features/Carel/static/images/close_folders.png";
-import clemenceau from "../features/Carel/static/images/clemenceau.png";
+import maroquinerie from "../features/Carel/static/images/storage/maroquinerie.png";
+import chaussures from "../features/Carel/static/images/storage/chaussures.png";
+import close_folders from "../features/Carel/static/images/storage/close_folders.png";
+import clemenceau from "../features/Carel/static/images/storage/clemenceau.png";
+
+// New Image 
+import elysee from "../features/Carel/static/images/storage/elysee.png";
+import mabillon from "../features/Carel/static/images/storage/mabillon.png";
+import maddie from "../features/Carel/static/images/storage/maddie.png";
+import madeleine_brodee from "../features/Carel/static/images/storage/madeleine_brodee.png";
+import madeleine from "../features/Carel/static/images/storage/madeleine.png";
+import mini_clemenceau from "../features/Carel/static/images/storage/mini_clemenceau.png";
+
+
 
 type BackgroundColor = typeof Colors[keyof typeof Colors];
 
@@ -19,7 +31,7 @@ interface carelProps {
 }
 
 export default function Carel({ setAcceuil, setLogoFanch, setTextColor }: carelProps) {
-  const { windowRef, concordeRef, apcRef, alesiaRef, StorageRef, maroquinerieRef, chaussuresRef, closeFoldersRef, clemenceauRef } = useFolders();
+  const { windowRef, concordeRef, bossaRef, alesiaRef, StorageRef, maroquinerieRef, chaussuresRef, closeFoldersRef, clemenceauRef, accessoiresLabelRef, accessoiresRef, miniClemenceauRef, madeleineRef, madeleineBrodeeRef, maddieRef, mabillonRef, elyseeRef } = useFolders();
 
   useEffect(() => {
     setAcceuil(Colors.Black);
@@ -33,12 +45,20 @@ export default function Carel({ setAcceuil, setLogoFanch, setTextColor }: carelP
     <div ref={windowRef} className="w-full h-screen relative overflow-hidden">
       <img ref={closeFoldersRef} src={close_folders} alt="Close Folders" className="absolute w-[70vw] h-auto"/>
       <img ref={StorageRef} src={Storage} alt="Storage" className="absolute w-[70vw] h-auto" />
-      <img ref={concordeRef} src={Concorde} data-folder="concorde" alt="Concorde" className="absolute w-[70vw] h-auto"/>
+      <img ref={accessoiresLabelRef} src={accessoires} data-folder="accessoires" alt="Accessoires" className="absolute w-[70vw] h-auto"/>
+      <img ref={accessoiresRef} src={accessoires_folder} data-folder="accessoires_folder" alt="Accessoires Folder" className="absolute w-[70vw] h-auto"/>
       <img ref={chaussuresRef} src={chaussures} data-folder="chaussures" alt="Chaussures" className="absolute w-[70vw] h-auto"/>
-      <img ref={apcRef} src={APC} data-folder="apc" alt="APC" className="absolute w-[70vw] h-auto"/>
+      <img ref={bossaRef} src={Bossa} data-folder="bossa" alt="Bossa" className="absolute w-[70vw] h-auto"/>
       <img ref={maroquinerieRef} src={maroquinerie} data-folder="maroquinerie" alt="Maroquinerie" className="absolute w-[70vw] h-auto"/>
-      <img ref={alesiaRef} src={Alesia} data-folder="alesia" alt="Alesia" className="absolute w-[70vw] h-auto"/>
+      <img ref={concordeRef} src={Concorde} data-folder="concorde" alt="Concorde" className="absolute w-[70vw] h-auto"/>
       <img ref={clemenceauRef} src={clemenceau} data-folder="clemenceau" alt="Clemenceau" className="absolute w-[70vw] h-auto"/>
+      <img ref={miniClemenceauRef} src={mini_clemenceau} data-folder="mini_clemenceau" alt="Mini Clemenceau" className="absolute w-[70vw] h-auto"/>
+      <img ref={alesiaRef} src={Alesia} data-folder="alesia" alt="Alesia" className="absolute w-[70vw] h-auto"/>
+      <img ref={madeleineRef} src={madeleine} data-folder="madeleine" alt="Madeleine" className="absolute w-[70vw] h-auto"/>
+      <img ref={maddieRef} src={maddie} data-folder="maddie" alt="Maddie" className="absolute w-[70vw] h-auto"/>
+      <img ref={madeleineBrodeeRef} src={madeleine_brodee} data-folder="madeleine_brodee" alt="Madeleine Brodee" className="absolute w-[70vw] h-auto"/>
+      <img ref={elyseeRef} src={elysee} data-folder="elysee" alt="Elysee" className="absolute w-[70vw] h-auto"/>
+      <img ref={mabillonRef} src={mabillon} data-folder="mabillon" alt="Mabillon" className="absolute w-[70vw] h-auto"/>
       {/*<img ref={defaultRef} src={defaultFolder} alt="Default Folders" className="absolute w-[70vw] h-auto" />*/}
     </div>
   );
